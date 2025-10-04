7844ac91a3a4a7df091476c90e576864cf75735a
JaswanthDara <techdepteducul.in>
2025-10-04 12:19:26 +0530

    Add react_frontend folder

--- database/site_safety_monitor_oops/node_backend/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-
-router.get('/', notificationController.getNotifications);
-router.put('/:id/read', notificationController.markAsRead);
-
-module.exports = router;
